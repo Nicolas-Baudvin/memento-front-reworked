08c1934018b9a1b1cdc8c77f03ea2a0bbc3e9870
unknown <nicolas.baudvin@gmail.com>
2021-02-20 14:20:46 +0100

    set up react router

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Login from "../Login";
 import Header from "../Header";
 
-const App = () => <div className="App">
-  <Header />
-  <Login />
-</div>;
+const App = () => (
+  <Router>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/connexion">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
