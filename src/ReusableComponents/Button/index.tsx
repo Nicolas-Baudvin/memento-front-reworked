390e45f1732ce207924e246f7d5ee80898a17175
unknown <nicolas.baudvin@gmail.com>
2021-02-22 15:40:25 +0100

    storing reusable components here

---
+++ src/ReusableComponents/Button/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+interface ButtonProps {
+    title: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ title }) => {
+    return <button className="button">{title}</button>
+};
+
+export default Button;
