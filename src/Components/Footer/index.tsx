b6fe0f3d18ce9a724c79bd0ae839a590cea72824
unknown <nicolas.baudvin@gmail.com>
2021-02-22 15:36:44 +0100

    WIP: Footer component

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content"></div>
+      <div className="footer-content">
+        <a href="#">About</a>
+        <a href="#">Support</a>
+        <a href="#">Contact</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
