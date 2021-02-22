de16565ab06a92770e87d0e3f6aed82c1b657bb8
unknown <nicolas.baudvin@gmail.com>
2021-02-22 15:36:56 +0100

    add particles

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 
 import Login from "../Login";
 import Header from "../Header";
+import Signup from "../Signup";
+import Home from "../Home";
+import Footer from "../Footer";
+
+// background
+import Particles from "./Particles";
 
 const App = () => (
   <Router>
     <div className="App">
+      <Particles />
       <Header />
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/connexion">
           <Login />
         </Route>
+        <Route exact path="/inscription">
+          <Signup />
+        </Route>
       </Switch>
+      <Footer />
     </div>
   </Router>
 );
