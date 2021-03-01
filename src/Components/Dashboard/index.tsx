55acf2f6d078cbbb93bd991ceaa9b94eb3822404
unknown <nicolas.baudvin@gmail.com>
2021-03-01 16:15:35 +0100

    Improve db component

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,22 +1,36 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { RootState } from "../../Store/reducer";
+import { getBoards } from "../../Store/Tabs/actions";
 import "./style.scss";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
   const { token } = useSelector((state: RootState) => state.user);
+  const { all } = useSelector((state: RootState) => state.boards);
   const history = useHistory();
 
   useEffect(() => {
     if (!token) {
-        history.push("/");
+      history.push("/");
+    } else {
+      dispatch(getBoards());
     }
-  }, [token]);
+  }, []);
+
   return (
     <div className="dashboard">
-      <div className="dashboard-tabs">
+      <div className="dashboard-block">
         <h2>Mes tableaux</h2>
+        <div className="dashboard-tabs">
+          {
+            all && all.map((board) => <div className="dashboard-tabs__item">
+              <img src={board.image} alt="board" />
+              <h3> {board.name} </h3>
+            </div>)
+          }
+        </div>
       </div>
     </div>
   );
