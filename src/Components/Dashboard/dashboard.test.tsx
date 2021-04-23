78f3261bc4da18b95a622df6ddde789020964a21
unknown <nicolas.baudvin@gmail.com>
2021-04-23 13:54:51 +0200

    tests

--- src/Components/Dashboard/dashboard.test.tsx
+++ src/Components/Dashboard/dashboard.test.tsx
@@ -1,30 +1,26 @@
 import { mount } from "enzyme";
 import { useSelector as useSelectorMock } from "react-redux";
-import React, { useReducer, useState as useStateMock } from "react";
 import Dashboard from ".";
 import {
-  deleteBoard,
   getBoards,
-  GET_BOARDS,
   newBoard,
-  NEW_BOARD,
 } from "../../Store/Tabs/actions";
 import { Board } from "../../Store/Tabs/types";
-import Modale from "../Modale";
 import { throwNewError } from "../../Store/Message/actions";
-import { initialState, reducer } from "./utils";
-import { renderHook } from "@testing-library/react-hooks";
-import { act } from "react-dom/test-utils";
 
+const url =
+  "https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+const alt = "space";
+const image = { url, alt };
+const title = "test2";
 const mockBoardData: Board = {
   title: "title",
   ownerID: "ownerID",
   owner: { username: "owner" },
-  image: { url: "url", alt: "alt" },
+  image,
   _id: "_id",
 };
 const mockDispatch = jest.fn();
-const setState = jest.fn();
 const mockHistoryPush = jest.fn();
 
 jest.mock("react-router-dom", () => ({
@@ -40,11 +36,6 @@ jest.mock("react-redux", () => ({
   useDispatch: () => mockDispatch,
 }));
 
-const url =
-  "https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
-const alt = "space";
-const image = { url, alt };
-const title = "test2";
 
 describe("<Dashboard />", () => {
   beforeEach(() => {
@@ -52,6 +43,9 @@ describe("<Dashboard />", () => {
       token: "string",
       all: [mockBoardData],
       current: mockBoardData,
+      _id: "_id",
+      username: "owner",
+      email: "email"
     }));
   });
 
@@ -72,14 +66,14 @@ describe("<Dashboard />", () => {
     const wrapper = mount(<Dashboard />);
     const deleteButton = wrapper.find(".delete");
     deleteButton.simulate("click", mockBoardData);
-    expect(wrapper.find(".modale")).toHaveLength(1);
+    expect(wrapper.find(".modale").length).toEqual(1);
   });
 
   it("should close the modale", () => {
     const wrapper = mount(<Dashboard />);
     wrapper.find(".delete").simulate("click", mockBoardData);
     wrapper.find(".modale-back").simulate("click");
-    expect(wrapper.find(".modale")).toHaveLength(0);
+    expect(wrapper.find(".modale").length).toEqual(0);
   });
 
   it("should push the board title to history", () => {
@@ -94,10 +88,21 @@ describe("<Dashboard />", () => {
     wrapper.find(".dashboard-create").simulate("click");
     const boardForm = wrapper.find(".dashboard-form");
     const input = wrapper.find(".dashboard-form-input");
-    input.simulate("change", { target: { value: "test2" } });
+    input.simulate("change", { target: { value: "title" } });
     boardForm.simulate("submit");
     expect(mockDispatch).toHaveBeenCalledWith(
       throwNewError("Veuillez selectionner une image")
     );
+  });
+
+  it("should call dispatch when submitting form", () => {
+    const wrapper = mount(<Dashboard />);
+    wrapper.find(".dashboard-create").simulate("click");
+    wrapper.find(".dashboard-form-pics-btn-0").simulate("click");
+    wrapper
+      .find(".dashboard-form-input")
+      .simulate("change", { target: { value: "title" } });
+    wrapper.find(".dashboard-form-input").simulate("submit");
+    expect(mockDispatch).toHaveBeenCalled();
   });
 });
