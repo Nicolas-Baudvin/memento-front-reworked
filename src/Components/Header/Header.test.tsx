b1984af162509fe5649e5a0d28fe627b8c7edd83
unknown <nicolas.baudvin@gmail.com>
2021-05-10 12:46:34 +0200

    add dispatch mock & selector mock

--- src/Components/Header/Header.test.tsx
+++ src/Components/Header/Header.test.tsx
@@ -1,16 +1,33 @@
-import { shallow } from 'enzyme'; 
-import Header from '.';
+import { shallow } from "enzyme";
+import { useSelector as useSelectorMock } from "react-redux";
+import Header from ".";
 
 const mockHistoryPush = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useHistory: () => ({
     push: mockHistoryPush,
-    location: { pathname: "connexion" }
+    location: { pathname: "connexion" },
   }),
 }));
 
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+
 describe("Header", () => {
+  beforeEach(() => {
+    (useSelectorMock as jest.Mock).mockImplementation(() => ({
+      token: "",
+      _id: "",
+      username: "",
+      email: "",
+    }));
+  });
+
   it("should render", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toBeTruthy();
@@ -19,11 +36,7 @@ describe("Header", () => {
   it("should redirect", () => {
     const wrapper = shallow(<Header />);
     const buttons = wrapper.find("button");
-    const redirects = [
-      "nouveautes",
-      "connexion",
-      "inscription"
-    ];
+    const redirects = ["../nouveautes", "../connexion", "../inscription"];
 
     buttons.forEach((button, i) => {
       button.simulate("click");
