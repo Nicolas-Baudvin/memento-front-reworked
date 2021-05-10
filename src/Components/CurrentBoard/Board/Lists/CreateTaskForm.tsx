1bdc30f8fb7e41aa504987aa8f48007a40146080
unknown <nicolas.baudvin@gmail.com>
2021-05-10 12:49:47 +0200

    wip: dispatch task creation

--- src/Components/CurrentBoard/Board/Lists/CreateTaskForm.tsx
+++ src/Components/CurrentBoard/Board/Lists/CreateTaskForm.tsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { List } from "../../../../Store/Tabs/types";
 
 interface CreateTaskFormProps {
-    list: List
+  list: List;
 }
 
 const CreateTaskForm = ({ list }: CreateTaskFormProps) => {
-    const [newTaskValue, setTaskValue] = useState("");
-    const handleChangeTaskValue = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-      setTaskValue(e.target.value);
-    return (
-      <form className="list-form" action="">
-        <textarea
-          value={newTaskValue}
-          onChange={handleChangeTaskValue}
-          name="task"
-          cols={30}
-          rows={10}
-        />
-        <button
-          style={{ backgroundColor: list.color }}
-          className="list-form-submit"
-        >
-          Créer Tâche
-        </button>
-      </form>
-    );
+  const dispatch = useDispatch();
+  const [newTaskValue, setTaskValue] = useState("");
+  const handleChangeTaskValue = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setTaskValue(e.target.value);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit} className="list-form" action="">
+      <textarea
+        value={newTaskValue}
+        onChange={handleChangeTaskValue}
+        name="task"
+        cols={30}
+        rows={10}
+      />
+      <button
+        style={{ backgroundColor: list.color }}
+        className="list-form-submit"
+        type="submit"
+      >
+        Créer Tâche
+      </button>
+    </form>
+  );
 };
 
 export default CreateTaskForm;
