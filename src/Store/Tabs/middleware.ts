e7e48569ab43fe90d4f4d97fa542867ff93b54ee
unknown <nicolas.baudvin@gmail.com>
2021-05-14 11:38:43 +0200

    add error handler for 403 to delete fetch

--- src/Store/Tabs/middleware.ts
+++ src/Store/Tabs/middleware.ts
@@ -16,246 +16,247 @@ import {
 } from "./actions";
 import { BoardActions, List } from "./types";
 
-const middleware: Middleware<{}, RootState> = (store) => (next) => async (
-  action: BoardActions
-) => {
-  switch (action.type) {
-    case UPDATE_BOARDS_LISTS: {
-      const { token, _id } = store.getState().user;
-      const { current } = store.getState().boards;
-      const listsUpdated = action.payload;
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_CHANGE_LIST_ORDER,
-          method: "patch",
-          data: {
-            _id,
-            listsUpdated,
-            boardID: current?._id
-          },
-          headers: {
-            Authorization: `Bearer ${token}`
+const middleware: Middleware<{}, RootState> =
+  (store) => (next) => async (action: BoardActions) => {
+    switch (action.type) {
+      case UPDATE_BOARDS_LISTS: {
+        const { token, _id } = store.getState().user;
+        const { current } = store.getState().boards;
+        const listsUpdated = action.payload;
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_CHANGE_LIST_ORDER,
+            method: "patch",
+            data: {
+              _id,
+              listsUpdated,
+              boardID: current?._id,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          store.dispatch(updateBoards(res.data.boards));
+          store.dispatch(newCurrentBoard(res.data.board));
+          next(action);
+        } catch (e) {
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+        }
+        break;
+      }
+      case CHANGE_LIST_NAME: {
+        const { token, _id } = store.getState().user;
+        const { current } = store.getState().boards;
+        const { list, newName: newTitle } = action.payload;
+
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_CHANGE_LIST_NAME,
+            method: "patch",
+            data: {
+              boardID: current?._id,
+              _id,
+              list,
+              newTitle,
+            },
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          });
+          store.dispatch(newCurrentBoard(res.data.board));
+          store.dispatch(updateBoards(res.data.boards));
+          next(action);
+        } catch (e) {
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+        }
+
+        break;
+      }
+      case DELETE_LIST: {
+        const { token, _id } = store.getState().user;
+        const { current } = store.getState().boards;
+        const list: List = action.payload;
+        try {
+          const res = await axios({
+            method: "delete",
+            url: process.env.REACT_APP_DELETE_LIST,
+            data: {
+              boardID: current?._id,
+              listID: list._id,
+              _id,
+            },
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          });
+          console.log(res.data);
+          store.dispatch(updateBoards(res.data.boards));
+          store.dispatch(newCurrentBoard(res.data.board));
+          next(action);
+        } catch (e) {
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+        }
+        break;
+      }
+      case NEW_LIST: {
+        const { token, _id } = store.getState().user;
+        const { current } = store.getState().boards;
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_CREATE_LIST,
+            method: "post",
+            data: {
+              _id,
+              ...action.payload,
+              boardID: current?._id,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          store.dispatch(updateBoards(res.data.boards));
+          store.dispatch(newCurrentBoard(res.data.board));
+          next(action);
+        } catch (e) {
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+        }
+        break;
+      }
+
+      case DELETE_BOARD: {
+        const { token, _id } = store.getState().user;
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_DELETE_BOARD,
+            method: "delete",
+            data: {
+              board: action.payload,
+              _id,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          store.dispatch(updateBoards(res.data.boards));
+
+          next(action);
+        } catch (e) {
+          if (e?.response?.status === 403) store.dispatch(logout());
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+        }
+        break;
+      }
+
+      case NEW_BOARD: {
+        const { token, _id, email, username } = store.getState().user;
+        const { image, title } = action.payload;
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_CREATE_BOARDS,
+            method: "post",
+            data: {
+              _id,
+              username,
+              email,
+              image,
+              title,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          store.dispatch(updateBoards(res.data.boards));
+          next(action);
+        } catch (e) {
+          if (e?.response?.status === 403) store.dispatch(logout());
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+          next(action);
+        }
+        break;
+      }
+
+      case GET_BOARDS: {
+        const { token, _id, email } = store.getState().user;
+
+        try {
+          const res = await axios({
+            url: process.env.REACT_APP_GET_BOARDS,
+            method: "post",
+            data: {
+              _id,
+              email,
+            },
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (res.data.boards?.length) {
+            store.dispatch(updateBoards(res.data.boards));
           }
-        });
-        store.dispatch(updateBoards(res.data.boards));
-        store.dispatch(newCurrentBoard(res.data.board));
-        next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
-      }
-      break;
-    }
-    case CHANGE_LIST_NAME: {
-      const { token, _id } = store.getState().user;
-      const { current } = store.getState().boards;
-      const { list, newName: newTitle } = action.payload;
-
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_CHANGE_LIST_NAME,
-          method: "patch",
-          data: {
-            boardID: current?._id,
-            _id,
-            list,
-            newTitle,
-          },
-          headers: {
-            authorization: `Bearer ${token}`,
-          },
-        });
-        store.dispatch(newCurrentBoard(res.data.board));
-        store.dispatch(updateBoards(res.data.boards));
-        next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
+          next(action);
+        } catch (e) {
+          if (e?.response?.status === 403) store.dispatch(logout());
+          if (e?.response?.data?.error)
+            store.dispatch(throwNewError(e.response.data.error));
+          else if (e?.response?.data?.errors)
+            store.dispatch(throwNewError(e.response.data.errors[0].msg));
+          else
+            store.dispatch(
+              throwNewError("Une erreur est survenue avec le serveur.")
+            );
+          next(action);
+        }
+        break;
       }
 
-      break;
-    }
-    case DELETE_LIST: {
-      const { token, _id } = store.getState().user;
-      const { current } = store.getState().boards;
-      const list: List = action.payload;
-      try {
-        const res = await axios({
-          method: "delete",
-          url: process.env.REACT_APP_DELETE_LIST,
-          data: {
-            boardID: current?._id,
-            listID: list._id,
-            _id,
-          },
-          headers: {
-            authorization: `Bearer ${token}`,
-          },
-        });
-        console.log(res.data);
-        store.dispatch(updateBoards(res.data.boards));
-        store.dispatch(newCurrentBoard(res.data.board));
+      default: {
         next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
+        break;
       }
-      break;
     }
-    case NEW_LIST: {
-      const { token, _id } = store.getState().user;
-      const { current } = store.getState().boards;
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_CREATE_LIST,
-          method: "post",
-          data: {
-            _id,
-            ...action.payload,
-            boardID: current?._id,
-          },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        store.dispatch(updateBoards(res.data.boards));
-        store.dispatch(newCurrentBoard(res.data.board));
-        next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
-      }
-      break;
-    }
-
-    case DELETE_BOARD: {
-      const { token, _id } = store.getState().user;
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_DELETE_BOARD,
-          method: "delete",
-          data: {
-            board: action.payload,
-            _id,
-          },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        store.dispatch(updateBoards(res.data.boards));
-
-        next(action);
-      } catch (e) {
-        // if (e?.response?.status === 403) store.dispatch(logout());
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
-      }
-      break;
-    }
-
-    case NEW_BOARD: {
-      const { token, _id, email, username } = store.getState().user;
-      const { image, title } = action.payload;
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_CREATE_BOARDS,
-          method: "post",
-          data: {
-            _id,
-            username,
-            email,
-            image,
-            title,
-          },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        store.dispatch(updateBoards(res.data.boards));
-        next(action);
-      } catch (e) {
-        if (e?.response?.status === 403) store.dispatch(logout());
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
-        next(action);
-      }
-      break;
-    }
-
-    case GET_BOARDS: {
-      const { token, _id, email } = store.getState().user;
-
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_GET_BOARDS,
-          method: "post",
-          data: {
-            _id,
-            email,
-          },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        store.dispatch(updateBoards(res.data.boards));
-        next(action);
-      } catch (e) {
-        if (e?.response?.status === 403) store.dispatch(logout());
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
-        next(action);
-      }
-      break;
-    }
-
-    default: {
-      next(action);
-      break;
-    }
-  }
-};
+  };
 
 export default middleware;
