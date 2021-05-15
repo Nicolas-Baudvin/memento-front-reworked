85960bda62a744109c78e1593950901aabce87fd
unknown <nicolas.baudvin@gmail.com>
2021-05-15 12:25:01 +0200

    Refacto

--- src/Store/Tabs/middleware.ts
+++ src/Store/Tabs/middleware.ts
@@ -1,116 +1,89 @@
-import axios from "axios";
 import { Middleware } from "redux";
-import { throwNewError } from "../Message/actions";
+import fetchData, { errorHandler } from "../../Utils/fetchData";
 import { RootState } from "../reducer";
-import { logout } from "../UserData/actions";
-import {
-  DELETE_BOARD,
-  GET_BOARDS,
-  NEW_BOARD,
-  updateBoards,
-} from "./actions";
+import { DELETE_BOARD, GET_BOARDS, NEW_BOARD, updateBoards } from "./actions";
 import { BoardActions } from "./types";
+
+const optsByRoute = (action: any, opts: any) => ({
+  delete: {
+    url: process.env.REACT_APP_DELETE_BOARD,
+    method: "delete",
+    data: {
+      board: action.payload,
+      _id: opts._id,
+    },
+    headers: {
+      Authorization: `Bearer ${opts.token}`,
+    },
+  },
+  create: {
+    url: process.env.REACT_APP_CREATE_BOARDS,
+    method: "post",
+    data: {
+      _id: opts._id,
+      username: opts.username,
+      email: opts._email,
+      image: action.payload.image,
+      title: action.payload.title,
+    },
+    headers: {
+      Authorization: `Bearer ${opts.token}`,
+    },
+  },
+  get: {
+    url: process.env.REACT_APP_GET_BOARDS,
+    method: "post",
+    data: {
+      _id: opts._id,
+      email: opts.email,
+    },
+    headers: {
+      Authorization: `Bearer ${opts.token}`,
+    },
+  },
+});
 
 const middleware: Middleware<{}, RootState> =
   (store) => (next) => async (action: BoardActions) => {
     switch (action.type) {
       case DELETE_BOARD: {
         const { token, _id } = store.getState().user;
+        const opts = optsByRoute(action, { token, _id })["delete"];
         try {
-          const res = await axios({
-            url: process.env.REACT_APP_DELETE_BOARD,
-            method: "delete",
-            data: {
-              board: action.payload,
-              _id,
-            },
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          store.dispatch(updateBoards(res.data.boards));
-
+          const data: any = await fetchData(opts);
+          store.dispatch(updateBoards(data.boards));
           next(action);
         } catch (e) {
-          if (e?.response?.status === 403) store.dispatch(logout());
-          if (e?.response?.data?.error)
-            store.dispatch(throwNewError(e.response.data.error));
-          else if (e?.response?.data?.errors)
-            store.dispatch(throwNewError(e.response.data.errors[0].msg));
-          else
-            store.dispatch(
-              throwNewError("Une erreur est survenue avec le serveur.")
-            );
+          errorHandler(e, store.dispatch, true);
         }
         break;
       }
 
       case NEW_BOARD: {
         const { token, _id, email, username } = store.getState().user;
-        const { image, title } = action.payload;
+        const opts = optsByRoute(action, { token, _id, username, email })[
+          "create"
+        ];
         try {
-          const res = await axios({
-            url: process.env.REACT_APP_CREATE_BOARDS,
-            method: "post",
-            data: {
-              _id,
-              username,
-              email,
-              image,
-              title,
-            },
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          store.dispatch(updateBoards(res.data.boards));
+          const data: any = await fetchData(opts);
+          store.dispatch(updateBoards(data.boards));
           next(action);
         } catch (e) {
-          if (e?.response?.status === 403) store.dispatch(logout());
-          if (e?.response?.data?.error)
-            store.dispatch(throwNewError(e.response.data.error));
-          else if (e?.response?.data?.errors)
-            store.dispatch(throwNewError(e.response.data.errors[0].msg));
-          else
-            store.dispatch(
-              throwNewError("Une erreur est survenue avec le serveur.")
-            );
-          next(action);
+          errorHandler(e, store.dispatch, true);
         }
         break;
       }
 
       case GET_BOARDS: {
         const { token, _id, email } = store.getState().user;
+        const opts = optsByRoute(action, { token, _id, email })["get"];
 
         try {
-          const res = await axios({
-            url: process.env.REACT_APP_GET_BOARDS,
-            method: "post",
-            data: {
-              _id,
-              email,
-            },
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          if (res.data.boards?.length) {
-            store.dispatch(updateBoards(res.data.boards));
-          }
+          const data: any = await fetchData(opts);
+          if (data.boards.length) store.dispatch(updateBoards(data.boards));
           next(action);
         } catch (e) {
-          if (e?.response?.status === 403) store.dispatch(logout());
-          if (e?.response?.data?.error)
-            store.dispatch(throwNewError(e.response.data.error));
-          else if (e?.response?.data?.errors)
-            store.dispatch(throwNewError(e.response.data.errors[0].msg));
-          else
-            store.dispatch(
-              throwNewError("Une erreur est survenue avec le serveur.")
-            );
-          next(action);
+          errorHandler(e, store.dispatch, true);
         }
         break;
       }
--- src/Store/UserData/middleware.ts
+++ src/Store/UserData/middleware.ts
@@ -1,75 +1,67 @@
-import axios from "axios";
 import { Middleware } from "redux";
-import { newMessage, throwNewError } from "../Message/actions";
+import fetchData, { errorHandler } from "../../Utils/fetchData";
+import { newMessage } from "../Message/actions";
 import { RootState } from "../reducer";
 import { CREATE_ACCOUNT, LOGOUT, USER_AUTH } from "./actions";
 
-const middleware: Middleware<{}, RootState> = (store) => (next) => async (
-  action
-) => {
-  switch (action.type) {
-    case CREATE_ACCOUNT: {
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_SIGNUP_URL,
-          method: "POST",
-          data: {
-            ...action.payload,
-          },
-        });
-        console.log(newMessage(res.data.message));
-        store.dispatch(newMessage(res.data.message));
-        next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
+const optsByRoute = (action: any) => ({
+  signup: {
+    url: process.env.REACT_APP_SIGNUP_URL,
+    method: "post",
+    headers: {},
+    data: { ...action.payload },
+  },
+  login: {
+    url: process.env.REACT_APP_LOGIN_URL,
+    method: "post",
+    headers: {},
+    data: { ...action.payload },
+  },
+});
+
+const middleware: Middleware<{}, RootState> =
+  (store) => (next) => async (action) => {
+    switch (action.type) {
+      case CREATE_ACCOUNT: {
+        const opts = optsByRoute(action)["signup"];
+        try {
+          const data: any = await fetchData(opts);
+          store.dispatch(newMessage(data.message));
+          next(action);
+        } catch (e) {
+          errorHandler(e, store.dispatch);
+        }
+        break;
       }
-      break;
-    }
-    case USER_AUTH: {
-      try {
-        const res = await axios({
-          url: process.env.REACT_APP_LOGIN_URL,
-          method: "post",
-          data: {
-            ...action.payload,
-          },
-        });
-        action.payload = { ...res.data, message: "Vous êtes connectés" };
-        store.dispatch(newMessage("Vous êtes connectés avec succès !"));
-        localStorage.setItem("tkn", res.data.token);
-        localStorage.setItem("_id", res.data._id);
-        localStorage.setItem("username", res.data.username);
-        next(action);
-      } catch (e) {
-        if (e?.response?.data?.error)
-          store.dispatch(throwNewError(e.response.data.error));
-        else if (e?.response?.data?.errors)
-          store.dispatch(throwNewError(e.response.data.errors[0].msg));
-        else
-          store.dispatch(
-            throwNewError("Une erreur est survenue avec le serveur.")
-          );
+      case USER_AUTH: {
+        const opts = optsByRoute(action)["login"];
+        try {
+          const data: any = await fetchData(opts);
+          action.payload = { ...data, message: "Vous êtes connectés" };
+          store.dispatch(newMessage("Vous êtes connectés avec succès !"));
+          storeUserDataToLocalStorage(data);
+          next(action);
+        } catch (e) {
+          errorHandler(e, store.dispatch);
+        }
+        break;
       }
-      break;
+      case LOGOUT: {
+        localStorage.clear();
+        next(action);
+        break;
+      }
+      default: {
+        next(action);
+        break;
+      }
     }
-    case LOGOUT: {
-      console.log("logout")
-      localStorage.clear();
-      next(action);
-      break;
-    }
-    default: {
-      next(action);
-      break;
-    }
-  }
+  };
+
+const storeUserDataToLocalStorage = (data: any) => {
+  localStorage.setItem("tkn", data.token);
+  localStorage.setItem("_id", data._id);
+  localStorage.setItem("username", data.username);
 };
 
 export default middleware;
