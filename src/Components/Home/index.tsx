1adf78fd06dab652b97a69bc8d9f03819e0a384e
unknown <nicolas.baudvin@gmail.com>
2021-05-17 14:31:51 +0200

    change text

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="home-text">
         <h1>Simple, Efficace & Professionnel</h1>
         <h2>
-          My Memento est indispensable dans l'organisation de votre entreprise.
+          Organisez-vous grâce à notre système de liste de tâche en temps réel.
         </h2>
         <button onClick={() => handleClickStart()} className="home-button">
           Commencer l'aventure
